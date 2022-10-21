520d97f0a8e52f2f0fae617d9b38fcd6d2c795db
taic-dev <c.t.o.taishi.0530@gmail.com>
2022-10-21 17:38:43 +0900

    worksの一覧テンプレートを作成

---
+++ src/components/object/works.js
@@ -0,0 +1,91 @@
+import Demo from "../../images/demo.png";
+
+export const WorksHP = [
+    {
+        id: 1,
+        img: Demo,
+        title: "id1タイトルです",
+        text: "1テキストテキストテキストテキスト",
+        year: "2022",
+        company: "会社名",
+        link: "https://www.google.com/?hl=ja"
+    },
+    {
+        id: 2,
+        img: Demo,
+        title: "id2タイトルです",
+        text: "2テキストテキストテキストテキスト",
+        year: "2021",
+        company: "個人制作",
+        link: "https://www.google.com/?hl=ja"
+    },
+    {
+        id: 3,
+        img: Demo,
+        title: "id3タイトルです",
+        text: "3テキストテキストテキストテキスト",
+        year: "2020",
+        company: "個人制作",
+        link: "https://www.google.com/?hl=ja"
+    },
+];
+
+export const WorksWEBSITE = [
+    {
+        id: 1,
+        img: Demo,
+        title: "id1タイトルです",
+        text: "1テキストテキストテキストテキスト",
+        year: "2022",
+        company: "会社名",
+        link: "https://www.google.com/?hl=ja"
+    },
+    {
+        id: 2,
+        img: Demo,
+        title: "id2タイトルです",
+        text: "2テキストテキストテキストテキスト",
+        year: "2021",
+        company: "個人制作",
+        link: "https://www.google.com/?hl=ja"
+    },
+    {
+        id: 3,
+        img: Demo,
+        title: "id3タイトルです",
+        text: "3テキストテキストテキストテキスト",
+        year: "2020",
+        company: "個人制作",
+        link: "https://www.google.com/?hl=ja"
+    },
+];
+
+export const Other = [
+    {
+        id: 1,
+        img: Demo,
+        title: "id1タイトルです",
+        text: "1テキストテキストテキストテキスト",
+        year: "2022",
+        company: "会社名",
+        link: "https://www.google.com/?hl=ja"
+    },
+    {
+        id: 2,
+        img: Demo,
+        title: "id2タイトルです",
+        text: "2テキストテキストテキストテキスト",
+        year: "2021",
+        company: "個人制作",
+        link: "https://www.google.com/?hl=ja"
+    },
+    {
+        id: 3,
+        img: Demo,
+        title: "id3タイトルです",
+        text: "3テキストテキストテキストテキスト",
+        year: "2020",
+        company: "個人制作",
+        link: "https://www.google.com/?hl=ja"
+    },
+];
--- src/components/templates/works.js
+++ src/components/templates/works.js
@@ -1,15 +1,109 @@
 import * as React from "react";
+import { Link } from "gatsby";
 import * as Styles from "../../styles/sass/module/_works.module.scss";
+import Img from "../../images/demo.png";
+
+import { WorksHP, WorksWEBSITE, Other } from "../object/works";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination } from "swiper";
+import "swiper/scss";
 
 const Works = () => {
   return (
     <section className="works">
       <div className="headline">
         <h2>Works</h2>
-        <span>実績 / 制作物</span>
+        <span>制作物</span>
       </div>
       <div className="content">
-          <p>テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト</p>
+        <h3>HP</h3>
+        <Swiper
+          className={Styles.works__list}
+          spaceBetween={30}
+          slidesPerView={2.5}
+          modules={[Pagination, Autoplay]}
+          autoplay={{
+            delay: 3500,
+            disableOnInteraction: false,
+          }}
+          loop={true}
+        >
+          {WorksHP.map((value, index) => {
+            return (
+              <SwiperSlide key={value.id} className={Styles.works__item}>
+                <Link to={value.link}>
+                  <img src={value.img} alt="画像" />
+                  <div className={Styles.works__desc}>
+                    <h4>{value.title}</h4>
+                    <p>{value.text}</p>
+                    <span>
+                      {value.year} / {value.company}
+                    </span>
+                  </div>
+                </Link>
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
+        <h3>WEBSITE</h3>
+        <Swiper
+          className={Styles.works__list}
+          spaceBetween={30}
+          slidesPerView={2.5}
+          modules={[Pagination, Autoplay]}
+          autoplay={{
+            delay: 3500,
+            disableOnInteraction: false,
+          }}
+          loop={true}
+        >
+          {WorksWEBSITE.map((value, index) => {
+            return (
+              <SwiperSlide key={value.id} className={Styles.works__item}>
+                <Link to={value.link}>
+                  <img src={value.img} alt="画像" />
+                  <div className={Styles.works__desc}>
+                    <h4>{value.title}</h4>
+                    <p>{value.text}</p>
+                    <span>
+                      {value.year} / {value.company}
+                    </span>
+                  </div>
+                </Link>
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
+        <h3>Other</h3>
+        <Swiper
+          className={Styles.works__list}
+          spaceBetween={30}
+          slidesPerView={2.5}
+          modules={[Pagination, Autoplay]}
+          autoplay={{
+            delay: 3500,
+            disableOnInteraction: false,
+          }}
+          loop={true}
+        >
+          {Other.map((value, index) => {
+            return (
+              <SwiperSlide key={value.id} className={Styles.works__item}>
+                <Link to={value.link}>
+                  <img src={value.img} alt="画像" />
+                  <div className={Styles.works__desc}>
+                    <h4>{value.title}</h4>
+                    <p>{value.text}</p>
+                    <span>
+                      {value.year} / {value.company}
+                    </span>
+                  </div>
+                </Link>
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
       </div>
     </section>
   );
