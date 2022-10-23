94e03d7eefec390f89a18a60a894d871918b4f6a
taic-dev <c.t.o.taishi.0530@gmail.com>
2022-10-23 13:54:23 +0900

    works部分のモーダル部分完成

--- src/components/object/works.js
+++ src/components/object/works.js
@@ -3,89 +3,191 @@ import Demo from "../../images/demo.png";
 export const WorksHP = [
     {
         id: 1,
+        name: "Confidence",
         img: Demo,
-        title: "id1タイトルです",
+        title: "PersonalGym Confidence HP制作",
         text: "1テキストテキストテキストテキスト",
         year: "2022",
-        company: "会社名",
-        link: "https://www.google.com/?hl=ja"
+        company: "PersonalGym Confidence 様",
+        detail:{
+            detailText: "パーソナルテキスト",
+            lang: [""],
+
+        }
+        
     },
     {
         id: 2,
+        name: "Remaker",
         img: Demo,
-        title: "id2タイトルです",
+        title: "株式会社Remaker HP制作",
         text: "2テキストテキストテキストテキスト",
-        year: "2021",
-        company: "個人制作",
-        link: "https://www.google.com/?hl=ja"
+        year: "2022",
+        company: "株式会社 Remaker 様",
+        detail:{
+            detailText: "Remakerテキスト",
+            lang: [""],
+
+        }
     },
     {
         id: 3,
+        name: "Tuda",
         img: Demo,
-        title: "id3タイトルです",
+        title: "株式会社ツダ企画 HP制作",
         text: "3テキストテキストテキストテキスト",
-        year: "2020",
-        company: "個人制作",
-        link: "https://www.google.com/?hl=ja"
+        year: "2021",
+        company: "株式会社 ツダ企画 様",
+        detail:{
+            detailText: "",
+            lang: [""],
+
+        }
+    },
+    {
+        id: 4,
+        name: "Setouchi",
+        img: Demo,
+        title: "有限会社せとうち通商 HP制作",
+        text: "3テキストテキストテキストテキスト",
+        year: "2021",
+        company: "有限会社 せとうち通商 様",
+        detail:{
+            detailText: "",
+            lang: [""],
+
+        }
     },
 ];
 
 export const WorksWEBSITE = [
     {
         id: 1,
+        name: "Hearbvest",
         img: Demo,
-        title: "id1タイトルです",
+        title: "Hearbvest フロントエンド開発",
         text: "1テキストテキストテキストテキスト",
-        year: "2022",
-        company: "会社名",
-        link: "https://www.google.com/?hl=ja"
+        year: "2021",
+        company: "株式会社 愛媛新聞社 様",
+        detail:{
+            detailText: "",
+            lang: [""],
+
+        }
     },
     {
         id: 2,
+        name: "Micropost",
         img: Demo,
-        title: "id2タイトルです",
+        title: "Micropost -ひとこと掲示板-",
         text: "2テキストテキストテキストテキスト",
-        year: "2021",
+        year: "2022",
         company: "個人制作",
-        link: "https://www.google.com/?hl=ja"
+        detail:{
+            detailText: "",
+            lang: [""],
+
+        }
     },
     {
         id: 3,
+        name: "NewsApp",
         img: Demo,
-        title: "id3タイトルです",
+        title: "News App",
         text: "3テキストテキストテキストテキスト",
         year: "2020",
         company: "個人制作",
-        link: "https://www.google.com/?hl=ja"
+        detail:{
+            detailText: "",
+            lang: [""],
+
+        }
+    },
+    {
+        id: 4,
+        name: "QuoteApp",
+        img: Demo,
+        title: "見積もり書作成 App",
+        text: "3テキストテキストテキストテキスト",
+        year: "2020",
+        company: "個人制作",
+        detail:{
+            detailText: "",
+            lang: [""],
+
+        }
+    },
+    {
+        id: 5,
+        name: "WeatherApp",
+        img: Demo,
+        title: "お天気 App",
+        text: "3テキストテキストテキストテキスト",
+        year: "2020",
+        company: "個人制作",
+        detail:{
+            detailText: "",
+            lang: [""],
+
+        }
     },
 ];
 
 export const Other = [
     {
         id: 1,
+        name: "TypingGame",
         img: Demo,
-        title: "id1タイトルです",
+        title: "タイピングゲーム -JS関数編-",
         text: "1テキストテキストテキストテキスト",
         year: "2022",
-        company: "会社名",
-        link: "https://www.google.com/?hl=ja"
+        company: "個人制作",
+        detail:{
+            detailText: "",
+            lang: [""],
+
+        }
     },
     {
         id: 2,
+        name: "MusicPlayer",
         img: Demo,
-        title: "id2タイトルです",
+        title: "Music Player",
         text: "2テキストテキストテキストテキスト",
         year: "2021",
         company: "個人制作",
-        link: "https://www.google.com/?hl=ja"
+        detail:{
+            detailText: "",
+            lang: [""],
+
+        }
     },
     {
         id: 3,
+        name: "Tetris",
         img: Demo,
-        title: "id3タイトルです",
+        title: "テトリス -Tetris-",
         text: "3テキストテキストテキストテキスト",
         year: "2020",
         company: "個人制作",
-        link: "https://www.google.com/?hl=ja"
+        detail:{
+            detailText: "",
+            lang: [""],
+
+        }
+    },
+    {
+        id: 4,
+        name: "GAS",
+        img: Demo,
+        title: "GASを使用した業務効率化",
+        text: "3テキストテキストテキストテキスト",
+        year: "2020",
+        company: "個人制作",
+        detail:{
+            detailText: "",
+            lang: [""],
+
+        }
     },
 ];
--- src/components/templates/works.js
+++ src/components/templates/works.js
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { Link } from "gatsby";
+import Modal from "react-modal";
 import * as Styles from "../../styles/sass/module/_works.module.scss";
 import Img from "../../images/demo.png";
 
@@ -9,7 +10,13 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Pagination } from "swiper";
 import "swiper/scss";
 
+Modal.setAppElement("#___gatsby");
+
 const Works = () => {
+  const [modalOpen, setModalOpen] = React.useState(false);
+
+  console.log(modalOpen);
+
   return (
     <section className="works">
       <div className="headline">
@@ -31,8 +38,12 @@ const Works = () => {
         >
           {WorksHP.map((value, index) => {
             return (
-              <SwiperSlide key={value.id} className={Styles.works__item}>
-                <Link to={value.link}>
+              <>
+                <SwiperSlide
+                  key={value.id}
+                  className={Styles.works__item}
+                  onClick={() => setModalOpen(value.name)}
+                >
                   <img src={value.img} alt="画像" />
                   <div className={Styles.works__desc}>
                     <h4>{value.title}</h4>
@@ -41,11 +52,38 @@ const Works = () => {
                       {value.year} / {value.company}
                     </span>
                   </div>
-                </Link>
-              </SwiperSlide>
+                </SwiperSlide>
+              </>
             );
           })}
         </Swiper>
+
+        {WorksHP.map((value) => {
+          return (
+            <Modal
+              isOpen={modalOpen == value.name}
+              onRequestClose={() => setModalOpen(false)}
+              overlayClassName={{
+                base: "overlay__base",
+                afterOpen: "overlay__after",
+                beforeClose: "overlay__before",
+              }}
+              className={{
+                base: "content__base",
+                afterOpen: "content__after",
+                beforeClose: "content__before",
+              }}
+              closeTimeoutMS={600}
+            >
+              <p>{value.detail.detailText}</p>
+              <p>modal</p>
+              <p>modal</p>
+              <p>modal</p>
+              <p>modal</p>
+            </Modal>
+          );
+        })}
+
         <h3>WEBSITE</h3>
         <Swiper
           className={Styles.works__list}
@@ -60,7 +98,11 @@ const Works = () => {
         >
           {WorksWEBSITE.map((value, index) => {
             return (
-              <SwiperSlide key={value.id} className={Styles.works__item}>
+              <SwiperSlide
+                key={value.id}
+                className={Styles.works__item}
+                onClick={() => setModalOpen(value.name)}
+              >
                 <Link to={value.link}>
                   <img src={value.img} alt="画像" />
                   <div className={Styles.works__desc}>
@@ -75,6 +117,33 @@ const Works = () => {
             );
           })}
         </Swiper>
+
+        {WorksWEBSITE.map((value) => {
+          return (
+            <Modal
+              isOpen={modalOpen == value.name}
+              onRequestClose={() => setModalOpen(false)}
+              overlayClassName={{
+                base: "overlay__base",
+                afterOpen: "overlay__after",
+                beforeClose: "overlay__before",
+              }}
+              className={{
+                base: "content__base",
+                afterOpen: "content__after",
+                beforeClose: "content__before",
+              }}
+              closeTimeoutMS={600}
+            >
+              <p>{value.detail.detailText}</p>
+              <p>modal</p>
+              <p>modal</p>
+              <p>modal</p>
+              <p>modal</p>
+            </Modal>
+          );
+        })}
+
         <h3>Other</h3>
         <Swiper
           className={Styles.works__list}
@@ -89,7 +158,11 @@ const Works = () => {
         >
           {Other.map((value, index) => {
             return (
-              <SwiperSlide key={value.id} className={Styles.works__item}>
+              <SwiperSlide
+                key={value.id}
+                className={Styles.works__item}
+                onClick={() => setModalOpen(value.name)}
+              >
                 <Link to={value.link}>
                   <img src={value.img} alt="画像" />
                   <div className={Styles.works__desc}>
@@ -104,6 +177,32 @@ const Works = () => {
             );
           })}
         </Swiper>
+
+        {Other.map((value) => {
+          return (
+            <Modal
+              isOpen={modalOpen == value.name}
+              onRequestClose={() => setModalOpen(false)}
+              overlayClassName={{
+                base: "overlay__base",
+                afterOpen: "overlay__after",
+                beforeClose: "overlay__before",
+              }}
+              className={{
+                base: "content__base",
+                afterOpen: "content__after",
+                beforeClose: "content__before",
+              }}
+              closeTimeoutMS={600}
+            >
+              <p>{value.detail.detailText}</p>
+              <p>modal</p>
+              <p>modal</p>
+              <p>modal</p>
+              <p>modal</p>
+            </Modal>
+          );
+        })}
       </div>
     </section>
   );
